6d1d913474405d434bdf3f727808d48ab5d67d0e
Vitor Greati <greati@ufrn.edu.br>
2017-11-23 15:24:39 -0300

    Update loader

---
+++ iox9/src/main/java/pso/secondphase/iox9/model/GeneralModelFactory.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pso.secondphase.iox9.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author vitorgreati
+ */
+public class GeneralModelFactory implements ModelAbstractFactory {
+
+    @Override
+    public Entity createEntity(String identifier) {
+        return new Entity(identifier);
+    }
+
+    @Override
+    public IORecord createIORecord(Entity entity, Date instant, IORecordType type) {
+        return new IORecord(entity, instant, type);
+    }
+    
+}
